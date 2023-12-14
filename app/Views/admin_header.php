45a403869b3761f21d15a1df5fe32d6903a35a6e
ThoriqSlebew <thoriqkhoirkhoir537@gmail.com>
2023-12-14 14:35:02 +0700

    add new features

--- app/Views/admin_header.php
+++ app/Views/admin_header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SPK UMKM</title>
+  <title>SPK PMT</title>
   <link rel="shortcut icon" type="image/png" href="dist/img/logomalang.png">
 
   <base href="<?= base_url("assets") ?>/">
